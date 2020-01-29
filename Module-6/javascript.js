033010f39f37eb76b5d68a9de985c18b41680c11
Warren Shehan <wwshehan@gmail.com>
2020-01-28 23:55:25 -0500

    Initial commit

---
+++ Module-6/javascript.js
@@ -0,0 +1,12 @@
+// When "Grow" is clicked.. Increase the size of the box.
+$("#button1").on("click", function() {
+	$("#box").animate({height:"+=35px", width:"+=35px"}, "fast");
+})
+ // When "Blue" the box becomes blue
+$("#button2").on("click", function() 
+{document.getElementById("box").style.backgroundColor ="Blue";
+})
+{document.getElementById("#button3").addEventListener("click", function(){
+    box.style.opacity = 0; 
+ })
+}
